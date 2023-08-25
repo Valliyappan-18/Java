cb47227fbb78e5961adeccb0a4b2c753a64d823a
Valliyappan-p <p.valliyappan02@gmail.com>
2023-08-26 02:00:01 +0530

    Pattern-Programs

---
+++ PatternPrgm.java
@@ -0,0 +1,43 @@
+public class PatternPrgm {
+    public static void main(String args[]) {
+        /*
+            1
+            22
+            333
+            4444
+            55555
+         */
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+        /*
+            1
+            12
+            123
+            1234
+            12345  
+         */
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        /*
+            *
+            **
+            ***
+            ****
+            *****
+         */
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
